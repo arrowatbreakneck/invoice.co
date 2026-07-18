48364bbfd5caddcb63847c7cc6e751207e7f2b98
arrowatbreakneck <srinivasraghav03@gmail.com>
2026-07-18 18:43:56 +0530

    corrections done in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { InvoiceProvider } from './components/invoiceContext.jsx'
+import { InvoiceProvider } from './components/InvoiceContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
